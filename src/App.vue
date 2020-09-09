6063e3d3d5a75e7e16c8af7345e0f7d399dc28b6
snorkle0 <snorkle0@gmail.com>
2020-09-09 20:36:15 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
+export default {
+    data: function() {
+        return {
+            quotes: [
+                'Simple quote to check out'
+            ],
+            maxQuotes: 10
+        }
+    },
+    methods: {
+        newQuote(quote) {
+            this.quotes.push(quote);
+        }
+    },
+    components: {
+        appQuoteGrid: QuoteGrid,
+        appNewQuote: NewQuote
+    }
+};
+</script>
+
+<style>
+
+</style>
