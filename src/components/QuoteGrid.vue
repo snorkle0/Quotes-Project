4d4038dbd11453f07400107bc6c28d580d8e7078
snorkle0 <snorkle0@gmail.com>
2020-09-09 21:04:30 +0200

    add progress bar

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <app-quote v-for="quote in quotes" v-bind:key="quote">{{ quote }}</app-quote>
+        <app-quote v-for="(quote, index) in quotes" v-bind:key="quote" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
     </div>
 </template>
 
@@ -10,6 +10,11 @@ export default {
     props: ['quotes'],
     components: {
         appQuote: Quote
+    },
+    methods: {
+        deleteQuote(index) {
+            this.$emit('quoteDeleted', index);
+        }
     }
 }
 </script>
